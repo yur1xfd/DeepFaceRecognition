4ea921546cbb6dfc193319aeb069fa223aea1e61
Roman Dyachenko <dyachenko.rr@yandex.ru>
2024-10-25 14:45:53 +0300

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -7,23 +7,23 @@ output_raw_dir = "output_raw"
 output_dir = "output"
 
 def test_preprocessing() -> None:
-    input_files = ([f[:-4] for f in os.listdir(input_raw_dir) if f.endswith('.txt')] +
-                   [f for f in os.listdir(input_raw_dir) if f.endswith('.wav')])
-    processed_files = {f[:-4] for f in os.listdir(input_dir) if f.endswith('.txt')}
+    input_files = ([f[:-4] for f in os.listdir(input_raw_dir) if f.endswith('.png')] +
+                   [f for f in os.listdir(input_raw_dir) if f.endswith('.jpg')])
+    processed_files = ([f[:-4] for f in os.listdir(input_dir) if f.endswith('.png')] +
+                   [f for f in os.listdir(input_dir) if f.endswith('.jpg')])
     
     for file_pattern in input_files:
         assert file_pattern in processed_files
 
 def test_processing() -> None:
-    input_files = ["response_" + f[:-4] for f in os.listdir(input_dir) if f.endswith('.txt')]
-    processed_files = {f[:-4] for f in os.listdir(output_raw_dir) if f.endswith('.txt')}
-    
-    for file_pattern in input_files:
-        assert file_pattern in processed_files
+    processed_files = ([for f in os.listdir(output_raw_dir))
+        assert "bounding_boxes.json" in processed_files
 
 def test_postprocessing() -> None:
-    input_files = [f[:-4] for f in os.listdir(output_raw_dir) if f.endswith('.txt')]
-    processed_files = {f[:-4] for f in os.listdir(output_dir) if f.endswith('.png')}
+    input_files = (["output_" + f for f in os.listdir(input_dir) if f.endswith('.jpg')] +
+                   ["output_" + f for f in os.listdir(input_dir) if f.endswith('.png')])
+    input_files = ([f for f in os.listdir(output_dir) if f.endswith('.jpg')] +
+                   [f for f in os.listdir(output_dir) if f.endswith('.png')])
     
     for file_pattern in input_files:
         assert file_pattern in processed_files
